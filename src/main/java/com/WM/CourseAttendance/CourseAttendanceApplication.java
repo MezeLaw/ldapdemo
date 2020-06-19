53298c58c55dd51f166707e16e8f18fb305102da
Martin Abogado <martin.abogado@walmart.com>
2020-06-19 12:17:04 -0300

    Primre commit.

---
+++ src/main/java/com/WM/CourseAttendance/CourseAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.WM.CourseAttendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WM/CourseAttendance/CourseAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WM.CourseAttendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
